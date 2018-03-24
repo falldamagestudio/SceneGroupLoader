9b82d133e0055564e536d5977972f9cb3a7f3a6b
Mikael Kalms <mikael@kalms.org>
2018-03-24 15:51:12 +0100

    Added a component wrapper for SceneGroupLoader

---
+++ SceneGroupLoaderComponent.cs
@@ -0,0 +1,27 @@
+﻿namespace SceneGroupLoader
+{
+    public class SceneGroupLoaderComponent
+    {
+        public SceneGroupLoader SceneGroupLoader = new SceneGroupLoader();
+
+        public void LoadSceneGroup(SceneGroup sceneGroup, SceneGroupLoader.OnDone onDone)
+        {
+            SceneGroupLoader.LoadSceneGroup(sceneGroup, onDone);
+        }
+
+        public void ActivateSceneGroup(SceneGroupLoader.SceneGroupHandle asyncSceneGroup, SceneGroupLoader.OnDone onDone)
+        {
+            SceneGroupLoader.ActivateSceneGroup(asyncSceneGroup, onDone);
+        }
+
+        public void UnloadSceneGroup(SceneGroupLoader.SceneGroupHandle asyncSceneGroup, SceneGroupLoader.OnDone onDone)
+        {
+            SceneGroupLoader.UnloadSceneGroup(asyncSceneGroup, onDone);
+        }
+
+        void Update()
+        {
+            SceneGroupLoader.UpdateStatus();
+        }
+    }
+}
