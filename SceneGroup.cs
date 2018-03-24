77ada636623a9996e31f50e065adfe051fb3ca79
Mikael Kalms <mikael@kalms.org>
2018-03-24 15:44:49 +0100

    Add context menu item for creating SceneGroups

--- SceneGroup.cs
+++ SceneGroup.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 namespace SceneGroupLoader
 {
+    [CreateAssetMenu(fileName = "SceneGroup", menuName = "SceneGroup", order = 1000)]
     public class SceneGroup : ScriptableObject
     {
         public List<string> Scenes;
